c3c2a8f921421e974985601d4f0e8a961ab15e59
Archisman Das <das.archisman2001@gmail.com>
2025-04-03 19:42:53 +0530

    Add files via upload

---
+++ for_loop.R
@@ -0,0 +1,14 @@
+#R FOR LOOP
+for (y in 1:10) {
+  print(y)
+}
+
+for (y in 1:10) {
+  print(paste("number: ",y))
+}
+
+
+f<-c("orange","apple","banana","graps","mango")
+for (i in f) {
+  print(f)
+} #5 times print the value cause the number of items within vector is fiva
