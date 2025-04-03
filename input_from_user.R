ea73c9840c218b784ecb781eaf13fd5feef3b765
Archisman Das <das.archisman2001@gmail.com>
2025-04-03 19:43:29 +0530

    Add files via upload

---
+++ input_from_user.R
@@ -0,0 +1,6 @@
+# R Take Input From User
+name<-readline(prompt="Enter your name: ")
+age<-readline(prompt = "Enter your age: ")
+print(paste("Hello my name is: ",name," and my age is: ",age))
+paste("hello","233","rum",sep="") #concate the series of strings
+paste0("hello","233","rum")
